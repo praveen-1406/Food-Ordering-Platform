5a852da67d18d60782afe3e33213bb0562de70cf
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-07-14 13:33:53 +0530

    Validate aceess token in Backend

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,11 @@
 import express, { RequestHandler } from "express";
 import MyUserController from "../controllers/MyUserController"
+import { jwtCheck } from "../middlewares/auth";
 const router=express.Router();
 
 // /api/my/user
 
-router.post("/",MyUserController.createCurrentUser as RequestHandler) 
+router.post("/",jwtCheck,MyUserController.createCurrentUser as RequestHandler) 
 
 
 export default router; 
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react"
+import { Auth0Provider, } from "@auth0/auth0-react"
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -7,17 +7,18 @@ type Props = {
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
 
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if (!domain || !clientId || !redirectUri) {
+    if (!domain || !clientId || !redirectUri || !audience) {
         throw new Error("Unable to initialize auth!")
     }
 
-    const onRedirectCallback=(appState?:AppState,user?:User)=>{
+    const onRedirectCallback = () => {
         // console.log("User:",user)
         // console.log("appState:",appState)
         navigate("/auth-callback")
@@ -29,6 +30,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
             clientId={clientId}
             authorizationParams={{
                 redirect_uri: redirectUri,
+                audience,
             }}
             onRedirectCallback={onRedirectCallback}
         >
