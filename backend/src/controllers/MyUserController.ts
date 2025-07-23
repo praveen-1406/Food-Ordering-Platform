e8582477b0877dead2e21e1414aadf3507105c71
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-07-23 23:09:15 +0530

    updateCurrentUser route: using jwt validation and parsing, and using express-validator

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,24 +1,52 @@
-import { Request,Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import User from "../models/user";
 
-export const createCurrentUser=async(req:Request,res:Response)=>{
+export const createCurrentUser = async (req: Request, res: Response) => {
     try {
-        const {auth0Id}=req.body;
-        const existingUser=await User.findOne({auth0Id})
-        
-        if(existingUser){
+        const { auth0Id } = req.body;
+        const existingUser = await User.findOne({ auth0Id })
+
+        if (existingUser) {
             return res.status(200).send("user already exists")
         }
-        const newUser=new User(req.body)
+        const newUser = new User(req.body)
         await newUser.save();
         res.status(201).json(newUser.toObject());
 
     } catch (error) {
         console.log(error);
-        return res.status(500).json({message:"Error creating user"})
+        return res.status(500).json({ message: "Error creating user" })
+    }
+}
+
+export const updateCurrentUser:RequestHandler = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const { name, addressLine1, country, city } = req.body;
+        const user = await User.findById(req.userId);
+
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
+
+        user.name = name;
+        user.addressLine1 = addressLine1;
+        user.city = city;
+        user.country = country;
+
+        await user.save();
+
+        res.send(user);
+        return;
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Error updating user" })
+        return;
     }
 }
 
 export default {
     createCurrentUser,
+    updateCurrentUser,
 }
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,11 +1,20 @@
 import express, { RequestHandler } from "express";
 import MyUserController from "../controllers/MyUserController"
-import { jwtCheck } from "../middlewares/auth";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateMyUserRequest } from "../middlewares/validation";
 const router=express.Router();
 
 // /api/my/user
 
-router.post("/",jwtCheck,MyUserController.createCurrentUser as RequestHandler) 
+router.post("/",jwtCheck,MyUserController.createCurrentUser as RequestHandler) ;
+
+router.put(
+    "/",
+    jwtCheck, 
+    jwtParse, 
+    validateMyUserRequest, 
+    MyUserController.updateCurrentUser as RequestHandler
+);
 
 
 export default router; 
