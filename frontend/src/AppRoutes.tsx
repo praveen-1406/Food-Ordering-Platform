17c1a028b8784ca935d0a9748fde1c987ea39c1a
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-07-31 13:15:28 +0530

    protect /user-profile route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/Layout"
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
@@ -17,27 +18,30 @@ const AppRoutes = () => {
         }
       />
 
-      <Route 
-        path="/auth-callback" 
+      <Route
+        path="/auth-callback"
         element={
           <AuthCallbackPage />
-        } 
+        }
       />
 
-      <Route 
-        path="/user-profile" 
-        element={
-          <Layout>
-            <UserProfilePage/>
-          </Layout>
-        } 
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
 
-      <Route 
-        path="*" 
+
+      <Route
+        path="*"
         element={
           <Navigate to="/" />
-        } 
+        }
       />
 
     </Routes>
