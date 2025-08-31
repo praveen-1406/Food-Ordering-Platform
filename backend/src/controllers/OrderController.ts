cb6dab08b4094e1c4ab3824334597110d011442a
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-01 01:55:42 +0530

    storing orders in database

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Stripe from "stripe";
 import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -33,6 +34,15 @@ const createCheckoutSession = async (req: Request, res: Response) => {
             throw new Error("Restaurant not found");
         }
 
+        const newOrder=new Order({
+            restaurant:restaurant,
+            user:req.userId,
+            status:"placed",
+            deliveryDetails:checkoutSessionRequest.deliveryDetails,
+            cartItems:checkoutSessionRequest.cartItems,
+            createdAt:new Date(),
+        })
+
         const lineItems = createLineItems(
             checkoutSessionRequest,
             restaurant.menuItems
@@ -40,16 +50,18 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 
         const session = await createSession(
             lineItems,
-            "TEST_ORDER_ID",
+            newOrder._id.toString(),
             restaurant.deliveryPrice,
             restaurant._id.toString()
         );
 
-        if(!session.url){
-            return res.status(500).json({message:"Error creating stripe session"});
+        if (!session.url) {
+            return res.status(500).json({ message: "Error creating stripe session" });
         }
 
-        res.json({url:session.url});
+        await newOrder.save();
+
+        res.json({ url: session.url });
 
     } catch (error: any) {
         console.log(error);
@@ -117,13 +129,13 @@ const createSession = async (
                 },
             },
         ],
-        mode:"payment",
-        metadata:{
+        mode: "payment",
+        metadata: {
             orderId,
             restaurantId,
         },
-        success_url:`${FRONTEND_URL}/order-status?success=true`,
-        cancel_url:`${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
+        success_url: `${FRONTEND_URL}/order-status?success=true`,
+        cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
     });
 
     return sessionData;
